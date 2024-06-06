2c482becc68b9ab6fd4bb7ee381eec7d5a42c261
Виталий Беяр <vitaliy.beyar@gmail.com>
2024-06-06 13:08:02 -0230

    added redirect to /login if clicked logout

--- components/auth/logout-button.tsx
+++ components/auth/logout-button.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { logout } from "@/actions/loguot";
+import { useRouter } from "next/router";
 import React from "react";
 
 interface LogOutButtonProps {
@@ -7,8 +8,12 @@ interface LogOutButtonProps {
 }
 
 const LogOutButton = ({ children }: LogOutButtonProps) => {
+  const router = useRouter();
   const onClick = () => {
+    router.push("/login");
     logout();
+
+
   };
   return (
     <span onClick={onClick} className="cursor-pointer">
