8f38fcc05dd3a3d952dc3cdd31b910077587e399
Виталий Беяр <vitaliy.beyar@gmail.com>
2024-06-06 11:21:09 -0230

    fixed back rout

--- components/auth/back-button.tsx
+++ components/auth/back-button.tsx
@@ -4,6 +4,8 @@ import React from "react";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
+const { NEXT_PUBLIC_APP_URL} = process.env;
+
 interface BackButtonProps {
   href: string;
   label: string;
@@ -12,7 +14,7 @@ interface BackButtonProps {
 export const BackButton = ({ href, label }: BackButtonProps) => {
   return (
     <Button variant="link" className="font-normal w-full" size="sm" asChild>
-      <Link href={href}>{label}</Link>
+      <Link href={`${NEXT_PUBLIC_APP_URL}${href}`}>{label}</Link>
     </Button>
   );
 };
